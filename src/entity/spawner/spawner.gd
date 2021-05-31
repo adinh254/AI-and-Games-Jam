14f5ca63de807ba1111a733de0ca97744e261a4c
Kedyn Macedonio <mkedyn@gmail.com>
2021-05-31 02:28:42 -0700

    random change

--- src/entity/spawner/spawner.gd
+++ src/entity/spawner/spawner.gd
@@ -9,16 +9,12 @@ func set_target(new_target: Node2D) -> void:
 	target = new_target
 
 func get_path_direction(unit_position: Vector2) -> Vector2:
-	return target.transform.get_origin()
+	return (target.position - unit_position).normalized()
 
 func spawn_enemies(enemies: int, initial_position: Vector2) -> void:
 	transform.origin = initial_position
 	
-	look_at(target.transform.get_origin())
-	
 	for i in enemies:
 		var new_enemy: Enemy = EnemyScene.instance()
-		
-		new_enemy.look_at(target.transform.get_origin())
 
 		add_child(new_enemy)
